9765090b658dc90b21b9582eba82de9b00ce7472
Anthon Hertz Bie <anthon_hb@hotmail.com>
2022-09-19 19:57:02 +0200

    variabler

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,9 @@
-
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner brugerinput = new Scanner(System.in);
-        int alder;
-        //brugerinput = new java.util.Scanner(System.in);
-        alder = brugerinput.nextInt();
 
-        if (alder > 18) {
-            System.out.println("Du er myndig");
-
-        } else
-            System.out.println("Du er ikke myndig");
+        System.out.println("Hello world!");
+        int a, b, c;
+        System.out.println("Kan emil?");
+        System.out.println("Halløjsa");
     }
 }
