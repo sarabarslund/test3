7681785486c94907c5cb6cf2f8751642e0ba65fe
CrazyCows <emil.s.simonsen@gmail.com>
2022-09-29 01:15:59 +0200

    Classes and structure slowly forming

--- src/spil/Main.java
+++ src/spil/Main.java
@@ -3,64 +3,81 @@ package spil;
 import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
-import gui_main.GUI;
 
-class allPlayers {
-    private int playerCount(){
-        Scanner sc = new Scanner(System.in);
-        int playerCount = sc.nextInt();
-        sc.nextLine();
-        return playerCount;
-    }
-    private String[] hiddenPlayers() {
-        String usrInput;
-        String[] players;
-        Scanner sc = new Scanner(System.in);
 
-        players = new String[this.playerCount()];
+class players {
+    private String usrInput;
+    private String[] players;
+    private Scanner sc = new Scanner(System.in);
+    public String[] playerNames(int playerCount) {
 
-        for (int i = 1; i <= this.playerCount(); i++) {
+        players = new String[playerCount];
+        for (int i = 1; i <= playerCount; i++) {
 
             System.out.println("Enter name of player number: " + i + ".");
             usrInput = sc.nextLine();
             players[i - 1] = usrInput;
-
         }
         return players;
     }
-    public String[] players(){
-        return this.hiddenPlayers();
+}
+class diceThrow {
+    int dice1 = new Random().nextInt(1, 7);
+    int dice2 = new Random().nextInt(1, 7);
+    int dices = dice1 + dice2;
     }
-    public int playerTotal(){
-        return this.playerCount();
+class turn {
+    private int start = 0, normalTurn, playerCount;
+    public void playerCount(int playerCount){
+        this.playerCount = playerCount;
+    }
+    public int currentTurn(int turn) {
+        if (start == 0) {
+            start += 1;
+            normalTurn = new Random().nextInt(playerCount);
+            normalTurn += 1;
+        }
+        else {
+            normalTurn += turn;
+        }
+        return normalTurn;
     }
 }
-class diceRolls {
-    private int[] HiddenDices() {
-        int dice1, dice2;
-        dice1 = new Random().nextInt(1, 7);
-        dice2 = new Random().nextInt(1, 7);
-        int[] dices = {dice1, dice2};
-        return dices;
-    }
-    public int[] dices(){
-        return this.HiddenDices();
+class gameRules {
+    private diceThrow dices = new diceThrow();
+    private int dice1 = dices.dice1;
+    private int dice2 = dices.dice1;
+    private int p1dice1, p1dice2, p2dice1, p2dice2;
+    String win = "Player wins";
+    public void getMainGame(int dice1,int dice2,int p1dice1,int p1dice2,int p2dice1,int p2dice2) {
+        int mainGame;
+        if (dice1 == 6 && dice2 == 6 && p1dice1 == 6 && p1dice2 == 6) {
+
+
+        }
     }
 }
 
-
-public class Main {
+public class Main{
     public static void main(String[] args) {
+        // This section only launches the objects except for playerCount as it's used in mutiple objects
+        int playerCount;
+        diceThrow dc = new diceThrow();
+        System.out.println(dc.dices);
+        players player = new players();
+        Scanner sc = new Scanner(System.in);
+        playerCount = sc.nextInt();
 
-        System.out.println("Hello, please enter the amount of players participating: ");
-        // Bruger en klasse til af definerer spillerne
-        allPlayers players = new allPlayers();
-        // Laver det om fra memmory kode til et array:
-        String[] names = players.players();
-        // Udskriver array, skal fjernes?
-        System.out.println(Arrays.toString(names));
-        System.out.println("Thank you, please proceed to the game:");
-        GUI gui = new GUI();
-        gui.showMessage("Player 1 press to start the game: ");
+        String[] me = player.playerNames(playerCount);
+        System.out.println(Arrays.toString(me));
+
+        if (playerCount == 0) {
+            System.out.println("NO");
+        }
+
+        turn tur = new turn();
+        tur.playerCount(playerCount);
+
+
     }
 }
--- src/test/test.java
+++ src/test/test.java
@@ -3,9 +3,37 @@ import java.util.Random;
 import java.util.Scanner;
 import java.util.Arrays;
 
+
+class mig {
+    private String ThisClass = "9";
+}
+class turn {
+    int start = 0, normalTurn;
+
+    int currentTurn(int turn) {
+        if (start == 0) {
+            start += 1;
+            normalTurn = new Random().nextInt(turn);
+            normalTurn += 1;
+        }
+        else {
+            normalTurn += turn;
+        }
+        return normalTurn;
+    }
+}
+
+
 public class test {
     public static void main(String[] args) {
+        turn tur = new turn();
+        System.out.println(tur.currentTurn(2));
+        System.out.println(tur.currentTurn(2));
+        System.out.println(tur.currentTurn(2));
+
+
 
 
         }
     }
+
